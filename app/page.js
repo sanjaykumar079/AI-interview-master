7aca253d9cec61ec52f173586155e0c771d116c1
sanjay kumar <153048840+sanjaykumar079@users.noreply.github.com>
2025-03-02 07:45:49 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -21,19 +21,26 @@ export default function Home() {
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">"Ace Every Interview with AI-Powered Precision!" 
 </p>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="/dashboard" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-slate-800 hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get Started
-                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </a>
+          
             <a
         href="https://resume-ats-scan.streamlit.app/"
         className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-slate-800 hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">        ResumeATS Pro
       </a>
       <br></br>
+      <a
+        href="https://apphrinterview-ca9n2nbbfp4rjznvkwnidm.streamlit.app/"
+        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-slate-800 hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">        Apply for Job
+      </a>
+      <a href="/dashboard" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-slate-800 hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get Started
+                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </a>
         </div>
 
     </div>
+ 
 </section>
+
 <section className="py-8 bg-white z-50 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
 {/* <h2 className="font-bold text-3xl">How it Works?</h2>
 <h2 className="text-md text-gray-500">Give mock interview in just 3 simplar easy step</h2> */}
